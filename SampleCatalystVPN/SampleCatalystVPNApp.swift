6d6b3e957ccbc3ab9a3a52fbd2959a4ad87b0c2d
Sander Bruens <sbruens@users.noreply.github.com>
2024-02-12 09:34:36 -0500

    Initial Commit

---
+++ SampleCatalystVPN/SampleCatalystVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCatalystVPNApp.swift
+//  SampleCatalystVPN
+//
+//  Created by Sander Bruens on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleCatalystVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
